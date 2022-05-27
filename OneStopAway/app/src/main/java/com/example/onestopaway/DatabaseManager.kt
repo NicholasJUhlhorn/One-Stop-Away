45b4875a7020385aa8b4e6a0317f404fae53b556
ilh <ilh@wwu.edu>
2022-05-27 14:41:54 -0700

    Added requested database functions

--- OneStopAway/app/src/main/java/com/example/onestopaway/DatabaseManager.kt
+++ OneStopAway/app/src/main/java/com/example/onestopaway/DatabaseManager.kt
@@ -8,8 +8,8 @@ import java.time.LocalTime
 
 class DatabaseManager(context: Context) : SQLiteOpenHelper(context, "Database", null, 1){
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE IF NOT EXISTS STOP(stop_number, name, latitude, longitude, locality, favorite)")
-        db?.execSQL("CREATE TABLE IF NOT EXISTS TRIP(id)")
+        db?.execSQL("CREATE TABLE IF NOT EXISTS STOP(stop_id, name, latitude, longitude, favorite)")
+        db?.execSQL("CREATE TABLE IF NOT EXISTS TRIP(id, head_sign)")
         db?.execSQL("CREATE TABLE IF NOT EXISTS ROUTE(id, arrival_time, departure_time, stop_id, favorite)")
     }
 
@@ -18,12 +18,12 @@ class DatabaseManager(context: Context) : SQLiteOpenHelper(context, "Database", 
     }
 
     //INSERT TO TABLES
-    fun insertStop(number: Int, nm: String, lat: Double, long: Double, loc: String, fav: Int){
-        writableDatabase.execSQL("INSERT INTO STOP VALUES($number, $nm, $lat, $long, $loc, $fav)")
+    fun insertStop(number: Int, nm: String, lat: Double, long: Double, fav: Int){
+        writableDatabase.execSQL("INSERT INTO STOP VALUES($number, $nm, $lat, $long, $fav)")
     }
 
-    fun insertTrip(id: Int){
-
+    fun insertTrip(id: Int, head: String){
+        writableDatabase.execSQL("INSERT INTO TRIP VALUES($id, $head)")
     }
 
     fun insertRoute(id: Int, at: String, dt: String, stop: Int, fav: Int){
@@ -42,8 +42,7 @@ class DatabaseManager(context: Context) : SQLiteOpenHelper(context, "Database", 
             row.add(cursor.getString(1))
             row.add(cursor.getDouble(2).toString())
             row.add(cursor.getDouble(3).toString())
-            row.add(cursor.getString(4))
-            row.add(cursor.getInt(5).toString())
+            row.add(cursor.getInt(4).toString())
 
             result.add(row)
         }
@@ -70,12 +69,26 @@ class DatabaseManager(context: Context) : SQLiteOpenHelper(context, "Database", 
         return result
     }
 
+    //Returns stop id based on stop name
+    fun getStopID(name: String): Int{
+        val id: Int
+
+        val cursor = writableDatabase.rawQuery("SELECT STOP.stop_id FROM STOP WHERE STOP.name = $name", null)
+
+        cursor.moveToNext()
+        id = cursor.getInt(0)
+
+        return id
+    }
+
     //Gets arrival times based on stop
     fun getArrivalTimeOnStop(stop_number: Int): List<String>{
         val result = mutableListOf<String>()
 
-        TODO("JOIN SQL TABLES FOR QUERY")
-        val cursor = writableDatabase.rawQuery("SELECT A.arrival_time FROM STOP", null)
+        val cursor = writableDatabase.rawQuery(
+            "SELECT A.arrival_time FROM (STOP INNER JOIN ROUTES ON STOP.stop_id = ROUTES.stop_id) AS A WHERE A.stop_id = $stop_number",
+            null)
+
         while(cursor.moveToNext()){
             result.add(cursor.getString(0))
         }
@@ -83,12 +96,26 @@ class DatabaseManager(context: Context) : SQLiteOpenHelper(context, "Database", 
         return result
     }
 
+    //Returns route id based on route name
+    fun getRouteID(name: String): Int{
+        val id: Int
+
+        val cursor = writableDatabase.rawQuery("SELECT TRIP.id FROM TRIP WHERE TRIP.head = $name", null)
+
+        cursor.moveToNext()
+        id = cursor.getInt(0)
+
+        return id
+    }
+
     //Gets stops based on route id
     fun getStopsOnRoute(id: Int): List<List<String>>{
         val result = mutableListOf<List<String>>()
 
-        TODO("JOIN SQL TABLES FOR QUERY")
-        val cursor = writableDatabase.rawQuery("SELECT A.name FROM STOP", null)
+        val cursor = writableDatabase.rawQuery(
+            "SELECT A.name FROM (STOP INNER JOIN ROUTES ON STOP.stop_id = ROUTES.stop_id) AS A WHERE A.stop_id = $id",
+            null)
+
         while(cursor.moveToNext()){
             result.add(listOf(cursor.getString(0)))
         }
